4f9c9f3fe53c96508a57f63dd788c2852ff29012
Ian Redden <iaredden@cisco.com>
2022-12-05 02:32:36 +0000

    updated pipfile from docker relay template

--- code/config.py
+++ code/config.py
@@ -4,17 +4,3 @@ import json
 class Config:
     settings = json.load(open('container_settings.json', 'r'))
     VERSION = settings["VERSION"]
-    CTIM_DEFAULTS = {
-        'schema_version': '1.1.12',
-    }
-
-    SOURCE = 'Cisco Meraki MX'
-    RELATIONS_DEFAULTS = {
-        "origin": SOURCE,
-        "relation": 'Connected_To'
-    }
-    SIGHTING_DEFAULTS = {
-        **CTIM_DEFAULTS,
-        'type': 'sighting',
-        'source': SOURCE,
-    }
